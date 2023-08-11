c661fbe3dd4c138826aae854b1ac892ade96085d
Tran Nhat Tien <103252635+nhattien2123@users.noreply.github.com>
2023-08-11 19:16:57 +0700

    Tien Commit

---
+++ client/src/components/DropdownPicker2.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+interface DataItem {
+  label: string;
+  value: number;
+}
+
+interface DataItems {
+  data: DataItem[]
+}
+
+const DropdownPicker: React.FC<DataItems> = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentValue, setCurrentValue] = useState<number | null>(null);
+
+  return (
+    <View style={{padding: 30, marginTop: 5}}>
+      <DropDownPicker
+        items={data}
+        open={isOpen}
+        setOpen={() => setIsOpen(!isOpen)}
+        value={currentValue}
+        setValue={(val) => setCurrentValue(val)}
+      />
+    </View>
+  )
+}
+
+export default DropdownPicker;
