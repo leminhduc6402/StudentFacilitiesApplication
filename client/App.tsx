52c81aee891d20024b4a9e4ae04946f97ab428b0
anvyidol <phanleho002@gmail.com>
2023-08-27 23:25:56 +0700

    update

--- client/App.tsx
+++ client/App.tsx
@@ -12,24 +12,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Header } from '@rneui/base';
 import CoursesRegistration from './src/views/CoursesRegistration';
 
-const Stack = createNativeStackNavigator();
-
 import { NativeRouter, Route, Link, Routes } from 'react-router-native';
 import UserProvider from './src/store/UserContext';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <TestScheduleDetail /> */}
-      {/* <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="Tuition" component={Tuition} />
-          <Stack.Screen name='TuitionDetail' component={TuitionDetail}/>
-        </Stack.Navigator>
-      </NavigationContainer> */}
-      <CoursesRegistration />
+      <NativeRouter>
+        <UserProvider>
+          <SafeAreaView style={styles.container}>
+            <Routes>
+              <Route path='/' Component={Main as any} />
+              <Route path='/login' Component={Login as any} />
+              <Route path='/tuition' Component={Tuition as any} />
+            </Routes>
+          </SafeAreaView>
+        </UserProvider>
+      </NativeRouter>
     </SafeAreaView>
   );
 }
