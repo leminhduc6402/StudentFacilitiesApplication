afe6025b82339d47ff96e7ba62e54ed63866bbfb
anvyidol <phanleho002@gmail.com>
2023-08-27 22:01:57 +0700

    update

--- client/App.tsx
+++ client/App.tsx
@@ -4,14 +4,13 @@ import Login from './src/views/Login';
 import Tuition from './src/views/Tuition/index';
 import Main from './src/views/Main';
 import TuitionDetail from './src/views/Tuition/TuitionDetail';
-import TestSchedule from './src/views/TestSchedule/index'
-import TestScheduleDetail from './src/views/TestSchedule/TestScheduleDetail'
+import TestSchedule from './src/views/TestSchedule/index';
+import TestScheduleDetail from './src/views/TestSchedule/TestScheduleDetail';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Header } from '@rneui/base';
 import CoursesRegistration from './src/views/CoursesRegistration';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -21,16 +20,17 @@ import UserProvider from './src/store/UserContext';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <TestScheduleDetail /> */}
-      {/* <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="Tuition" component={Tuition} />
-          <Stack.Screen name='TuitionDetail' component={TuitionDetail}/>
-        </Stack.Navigator>
-      </NavigationContainer> */}
-      <CoursesRegistration />
+      <NativeRouter>
+        <UserProvider>
+          <SafeAreaView style={styles.container}>
+            <Routes>
+              <Route path='/' Component={Main as any} />
+              <Route path='/login' Component={Login as any} />
+              <Route path='/tuition' Component={Tuition as any} />
+            </Routes>
+          </SafeAreaView>
+        </UserProvider>
+      </NativeRouter>
     </SafeAreaView>
   );
 }
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,50 +1,52 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import useUserContext from '../hook/useUserContext';
 
 const header = () => {
+  const [user, setUser] = useUserContext();
+
   return (
     <View style={styles.backgroungBar}>
-        <Image source={require("../images/user.png")} style={styles.picture} />
-        <View style={styles.textContainer}>
-          <Text style={styles.textUI}>Bùi Thanh Tâm</Text>
-          <Text style={styles.textUI}>2051052118</Text>
-        </View>
-        <Image source={require("../images/bell.png")} style={styles.bell} />
+      <Image source={require('../images/user.png')} style={styles.picture} />
+      <View style={styles.textContainer}>
+        <Text style={styles.textUI}>{user.fullName}</Text>
+        <Text style={styles.textUI}>{user.username}</Text>
       </View>
-  )
-}
+      <Image source={require('../images/bell.png')} style={styles.bell} />
+    </View>
+  );
+};
 
-export default header
+export default header;
 
 const styles = StyleSheet.create({
-    backgroungBar: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#0C56D0',
-        borderBottomRightRadius: 30,
-        borderBottomLeftRadius: 30,
-        height: 90,
-        width: '100%',
-        paddingHorizontal: 20,
-    },
-    picture: {
-        width: 50,
-        height: 50,
-        marginRight: 10,
-    },
-    textContainer: {
-        marginLeft: 20,
-    },
-    textUI: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    bell:{
-        width: 35,
-        height: 35,
-        marginLeft: 95,
-        marginBottom: 10,
-    },
-    
-})
+  backgroungBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#0C56D0',
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    height: 90,
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  picture: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  textContainer: {
+    marginLeft: 20,
+  },
+  textUI: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  bell: {
+    width: 35,
+    height: 35,
+    marginLeft: 95,
+    marginBottom: 10,
+  },
+});
