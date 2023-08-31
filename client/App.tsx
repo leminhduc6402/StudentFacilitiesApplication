c52fbdd0fefb00e04c837f2158694339bfd8579b
Tran Nhat Tien <103252635+nhattien2123@users.noreply.github.com>
2023-09-01 02:49:30 +0700

    update sort 2/3

--- client/App.tsx
+++ client/App.tsx
@@ -14,6 +14,7 @@ import ScoreResult from './src/views/ScoreResult';
 import LecturerMain from './src/views/Lecturer/Main';
 import ScoreInp from './src/views/Lecturer/ScoreInp';
 import GetClass from './src/views/Lecturer/GetClass';
+import DropdownProvider from './src/store/DropdownContext';
 
 export default function App() {
   return (
@@ -22,40 +23,42 @@ export default function App() {
         <UserProvider>
           <CourseProvider>
             <HistoryProvider>
-              <SafeAreaView style={styles.container}>
-                <Routes>
-                  {/* student */}
-                  <Route path={routes.HOME} Component={Main as any} />
-                  <Route path={routes.LOGIN} Component={Login as any} />
-                  <Route path={routes.TUITION} Component={Tuition as any} />
-                  <Route
-                    path={routes.COURSE_REGISTRATION}
-                    Component={CoursesRegistration as any}
-                  />
-                  <Route
-                    path={routes.COURSE_REGISTRATION_DETAIL}
-                    Component={CoursesRegistrationDetail as any}
-                  />
-                  <Route
-                    path={routes.SCORE_RESULT}
-                    Component={ScoreResult as any}
-                  />
+              <DropdownProvider>
+                <SafeAreaView style={styles.container}>
+                  <Routes>
+                    {/* student */}
+                    <Route path={routes.HOME} Component={Main as any} />
+                    <Route path={routes.LOGIN} Component={Login as any} />
+                    <Route path={routes.TUITION} Component={Tuition as any} />
+                    <Route
+                      path={routes.COURSE_REGISTRATION}
+                      Component={CoursesRegistration as any}
+                    />
+                    <Route
+                      path={routes.COURSE_REGISTRATION_DETAIL}
+                      Component={CoursesRegistrationDetail as any}
+                    />
+                    <Route
+                      path={routes.SCORE_RESULT}
+                      Component={ScoreResult as any}
+                    />
 
-                  {/* lecturer */}
-                  <Route
-                    path={routes.LECTURER_HOME}
-                    Component={LecturerMain as any}
-                  />
-                  <Route
-                    path={routes.LECTURER_SCORE_INP}
-                    Component={ScoreInp as any}
-                  />
-                  <Route
-                    path={routes.LECTURER_GET_CLASS}
-                    Component={GetClass as any}
-                  />
-                </Routes>
-              </SafeAreaView>
+                    {/* lecturer */}
+                    <Route
+                      path={routes.LECTURER_HOME}
+                      Component={LecturerMain as any}
+                    />
+                    <Route
+                      path={routes.LECTURER_SCORE_INP}
+                      Component={ScoreInp as any}
+                    />
+                    <Route
+                      path={routes.LECTURER_GET_CLASS}
+                      Component={GetClass as any}
+                    />
+                  </Routes>
+                </SafeAreaView>
+              </DropdownProvider>
             </HistoryProvider>
           </CourseProvider>
         </UserProvider>
---
+++ client/src/store/DropdownContext/Provider.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Context, Value, initialValue } from './Context';
+
+function DropdownProvider({children} : React.PropsWithChildren) {
+    const [currentValueTop, setCurrentValueTop] = useState(null);
+    const [currentValueBottom, setCurrentValueBottom] = useState(null);
+
+    return (
+      <Context.Provider value={[currentValueTop, setCurrentValueTop, currentValueBottom, setCurrentValueBottom] as any}>
+        {children}
+      </Context.Provider>
+    );
+  }
+
+export default DropdownProvider;
